5eaa9ee42e81a3d7f899cae5faf15c4e8a126ea5
Allison Chan <allisonchan@Allisons-MacBook-Pro-3.local>
2019-03-04 23:30:48 -0800

    updated lcd file for 4 bit

--- Makefile
+++ Makefile
@@ -1,9 +1,8 @@
-DEVICE     = atmega328p
-CLOCK      = 7372800
+DEVICE = atmega328p
+CLOCK = 9830400
 PROGRAMMER = -c usbtiny -P usb
-OBJECTS    = lcd.o
-FUSES      = -U hfuse:w:0xd9:m -U lfuse:w:0xe0:m
-
+OBJECTS = lcd.o
+FUSES = -U hfuse : w :0 xd9 : m -U lfuse : w :0 xe0 : m
 # Fuse Low Byte = 0xe0   Fuse High Byte = 0xd9   Fuse Extended Byte = 0xff
 # Bit 7: CKDIV8  = 1     Bit 7: RSTDISBL  = 1    Bit 7:
 #     6: CKOUT   = 1         6: DWEN      = 1        6:
--- lcd.c
+++ lcd.c
@@ -1,8 +1,8 @@
 /*************************************************************
-*       at328-4.c - Demonstrate interface to a parallel LCD display
+*       at328-5.c - Demonstrate interface to a parallel LCD display
 *
 *       This program will print a message on an LCD display
-*       using the 8-bit wide interface method
+*       using the 4-bit wide interface method
 *
 *       PORTB, bit 4 (0x10) - output to RS (Register Select) input of display
 *              bit 3 (0x08) - output to R/W (Read/Write) input of display
@@ -22,15 +22,26 @@
 * 01/15/08 A. Weber    More changes to the demo board
 * 02/12/08 A. Weber    Changed 2nd LCD line address
 * 04/22/08 A. Weber    Added "one" variable to make warning go away
-* 04/08/11 A. Weber    Adapted for ATmega168
+* 04/15/11 A. Weber    Adapted for ATmega168
+* 01/30/12 A. Weber    Moved the LCD strings into ROM
 * 02/27/12 A. Weber    Corrected port bit assignments above
 * 11/18/13 A. Weber    Renamed for ATmega328P
-* 04/10/15 A. Weber    Extended "E" pulse
+* 04/10/15 A. Weber    Extended "E" pulse, renamed strout to strout_P
 * 05/06/17 A. Weber    Change to use new LCD routines
 *************************************************************/
 
 #include <avr/io.h>
 #include <util/delay.h>
+#include <avr/pgmspace.h>
+
+/*
+  The NIBBLE_HIGH condition determines which PORT bits are used to
+  transfer data to data bits 4-7 of the LCD.
+  If LCD data bits 4-7 are connected to PORTD, bits 4-7, define NIBBLE_HIGH
+  If LCD data bits 4-7 are connected to PORTD, bits 2-3 and PORTB bits 0-1,
+  don't define NIBBLE_HIGH.
+*/
+#define NIBBLE_HIGH                 // Use bits 4-7 for talking to LCD
 
 void lcd_init(void);
 void lcd_moveto(unsigned char, unsigned char);
@@ -38,36 +49,64 @@ void lcd_stringout(char *);
 void lcd_writecommand(unsigned char);
 void lcd_writedata(unsigned char);
 void lcd_writebyte(unsigned char);
+void lcd_writenibble(unsigned char);
 void lcd_wait(void);
+void lcd_stringout_P(char *);
 
-char str1[] = ">>> at328-4.c <<78901234";
-char str2[] = ">> USC EE459L <<78901234";
-
-#define USE_BUSY_FLAG  1
+/*
+  Use the "PROGMEM" attribute to store the strings in the ROM
+  instead of in RAM.
+*/
+#ifdef NIBBLE_HIGH
+const unsigned char str1[] PROGMEM = ">> at328-5.c hi <<901234";
+#else
+const unsigned char str1[] PROGMEM = ">> at328-5.c lo <<901234";
+#endif
+const unsigned char str2[] PROGMEM = ">> USC EE459L <<78901234";
 
 #define LCD_RS          (1 << PB4)
 #define LCD_RW          (1 << PB3)
 #define LCD_E           (1 << PB2)
 #define LCD_Bits        (LCD_RS|LCD_RW|LCD_E)
 
+#ifdef NIBBLE_HIGH
+#define LCD_Data_D     0xf0     // Bits in Port D for LCD data
+#define LCD_Status     0x80     // Bit in Port D for LCD busy status
+#else
 #define LCD_Data_B     0x03     // Bits in Port B for LCD data
-#define LCD_Data_D     0xfc     // Bits in Port D for LCD data
+#define LCD_Data_D     0x0c     // Bits in Port D for LCD data
 #define LCD_Status     (1 << PD7) // Bit in Port D for LCD busy status
+#endif
 
 int main(void) {
 
     lcd_init();                 // Initialize the LCD display
 
     lcd_moveto(0, 0);
-    lcd_stringout(str1);        // Print string on line 1
+    lcd_stringout_P((char *)str1);      // Print string on line 1
 
     lcd_moveto(1, 0);
-    lcd_stringout(str2);        // Print string on line 2
+    lcd_stringout_P((char *)str2);      // Print string on line 2
 
     while (1) {                 // Loop forever
     }
 
     return 0;   /* never reached */
+}
+
+/*
+  lcd_stringout_P - Print the contents of the character string "s" starting at LCD
+  RAM location "x" where the string is stored in ROM.  The string must be
+  terminated by a zero byte.
+*/
+void lcd_stringout_P(char *s)
+{
+    unsigned char ch;
+
+    /* Use the "pgm_read_byte()" routine to read the date from ROM */
+    while ((ch = pgm_read_byte(s++)) != '\0') {
+        lcd_writedata(ch);             // Output the next character
+    }
 }
 
 /*
@@ -77,24 +116,31 @@ int main(void) {
 */
 void lcd_init(void)
 {
+#ifdef NIBBLE_HIGH
+    DDRD |= LCD_Data_D;         // Set PORTD bits 4-7 for output
+#else
     DDRB |= LCD_Data_B;         // Set PORTB bits 0-1 for output
-    DDRD |= LCD_Data_D;         // Set PORTD bits 2-7 for output
+    DDRD |= LCD_Data_D;         // Set PORTD bits 2-3 for output
+#endif
     DDRB |= LCD_Bits;           // Set PORTB bits 2, 3 and 4 for output
 
     PORTB &= ~LCD_RS;           // Clear RS for command write
 
     _delay_ms(15);              // Delay at least 15ms
 
-    lcd_writebyte(0x30);        // Send 00110000, set for 8-bit interface
+    lcd_writenibble(0x30);      // Send 00110000, set for 8-bit interface
     _delay_ms(5);               // Delay at least 4msec
 
-    lcd_writebyte(0x30);        // Send 00110000, set for 8-bit interface
+    lcd_writenibble(0x30);      // Send 00110000, set for 8-bit interface
     _delay_us(120);             // Delay at least 100usec
 
-    lcd_writebyte(0x30);        // Send 00110000, set for 8-bit interface
+    lcd_writenibble(0x30);      // Send 00110000, set for 8-bit interface
     _delay_ms(2);
 
-    lcd_writecommand(0x38);     // Function Set: 8-bit interface, 2 lines
+    lcd_writenibble(0x20);      // Send 00100000, set for 4-bit interface
+    _delay_ms(2);
+
+    lcd_writecommand(0x28);     // Function Set: 4-bit interface, 2 lines
 
     lcd_writecommand(0x0f);     // Display and cursor on
 }
@@ -146,10 +192,24 @@ void lcd_writedata(unsigned char x)
 */
 void lcd_writebyte(unsigned char x)
 {
+    lcd_writenibble(x);
+    lcd_writenibble(x << 4);
+}
+
+/*
+  lcd_writenibble - Output a 4-bit nibble to the LCD
+*/
+void lcd_writenibble(unsigned char x)
+{
+#ifdef NIBBLE_HIGH
+    PORTD &= ~LCD_Data_D;
+    PORTD |= (x & LCD_Data_D);  // Put high 4 bits of data in PORTD
+#else
     PORTB &= ~LCD_Data_B;
     PORTB |= (x & LCD_Data_B);  // Put low 2 bits of data in PORTB
     PORTD &= ~LCD_Data_D;
-    PORTD |= (x & LCD_Data_D);  // Put high 6 bits of data in PORTD
+    PORTD |= (x & LCD_Data_D);  // Put high 2 bits of data in PORTD
+#endif
     PORTB &= ~(LCD_RW|LCD_E);   // Set R/W=0, E=0
     PORTB |= LCD_E;             // Set E to 1
     PORTB |= LCD_E;             // Extend E pulse > 230ns
@@ -164,10 +224,15 @@ void lcd_wait()
 #ifdef USE_BUSY_FLAG
     unsigned char bf;
 
+#ifdef NIBBLE_HIGH
+    PORTD &= ~LCD_Data_D;       // Set for no pull ups
+    DDRD &= ~LCD_Data_D;        // Set for input
+#else
     PORTB &= ~LCD_Data_B;       // Set for no pull ups
     PORTD &= ~LCD_Data_D;
     DDRB &= ~LCD_Data_B;        // Set for input
     DDRD &= ~LCD_Data_D;
+#endif
 
     PORTB &= ~(LCD_E|LCD_RS);   // Set E=0, R/W=1, RS=0
     PORTB |= LCD_RW;
@@ -175,12 +240,18 @@ void lcd_wait()
     do {
         PORTB |= LCD_E;         // Set E=1
         _delay_us(1);           // Wait for signal to appear
-        bf = PIND & LCD_Status; // Read status bit
+        bf = PIND & LCD_Status; // Read status register high bits
         PORTB &= ~LCD_E;        // Set E=0
+	PORTB |= LCD_E;         // Need to clock E a second time to fake
+	PORTB &= ~LCD_E;        //   getting the status register low bits
     } while (bf != 0);          // If Busy (PORTD, bit 7 = 1), loop
 
+#ifdef NIBBLE_HIGH
+    DDRD |= LCD_Data_D;         // Set PORTD bits for output
+#else
     DDRB |= LCD_Data_B;         // Set PORTB, PORTD bits for output
     DDRD |= LCD_Data_D;
+#endif
 #else
     _delay_ms(2);		// Delay for 2ms
 #endif
